60018eabfe367129a131e27cb3547ccc0c749963
michaelj916 <46703736+michaelj916@users.noreply.github.com>
2020-09-08 13:20:22 -0700

    Updates

---
+++ precache-manifest.97bfd39c8effc6dd6a917711dbd2c657.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e26ea4e63920c51ed4c749882783d8e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a69ead66eddb27671672",
+    "url": "/static/css/main.ef42a8e6.chunk.css"
+  },
+  {
+    "revision": "852bf9bd473ee1fe4d16",
+    "url": "/static/js/2.f146625a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.f146625a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a69ead66eddb27671672",
+    "url": "/static/js/main.c9b6a594.chunk.js"
+  },
+  {
+    "revision": "e173dfe45a061c6e2715",
+    "url": "/static/js/runtime-main.20a4ba51.js"
+  },
+  {
+    "revision": "96872729ea83bd4fff699c4149dc7960",
+    "url": "/static/media/covid.96872729.png"
+  }
+]);
